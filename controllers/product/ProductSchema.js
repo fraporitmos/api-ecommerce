45b1ef143a0f4eac8679d3e54844e87c9f34793b
fraporitmos <fraporitmos>
2024-11-22 21:21:32 -0500

    product and category schema

---
+++ controllers/product/ProductSchema.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const ProductSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    stock: Number,
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    images: String
+});
